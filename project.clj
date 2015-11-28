63e00924cf77873f75a9650c88451e7a8c322cd6
Alex Kyllo <alex.kyllo@gmail.com>
2015-11-27 21:32:35 -0800

    add csv capability and use my fork of yesql to allow :as-arrays? keyword

--- project.clj
+++ project.clj
@@ -4,14 +4,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [yesql "0.5.1"]
+                 [org.clojars.alexkyllo/yesql "0.5.2"]
                  ;;[com.h2database/h2 "1.4.188"]
                  [org.postgresql/postgresql "9.4-1201-jdbc41"]
                  [com.draines/postal "1.11.3"]
                  [org.clojure/tools.nrepl "0.2.12"]
                  [schejulure "1.0.1"]
                  [hiccup "1.0.5"]
-                 [environ "1.0.1"]]
+                 [environ "1.0.1"]
+                 [tempfile "0.2.0"]
+                 [org.clojure/data.csv "0.1.3"]]
   :main ^:skip-aot sqlmail.core
   :target-path "target/%s"
   :plugins [[cider/cider-nrepl "0.9.1"]
--- src/sqlmail/core.clj
+++ src/sqlmail/core.clj
@@ -4,24 +4,54 @@
             [postal.core :refer [send-message]]
             [schejulure.core :refer [schedule]]
             [hiccup.core :refer [html]]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [clojure.data.csv :refer [write-csv]]
+            [clojure.java.io :refer [writer]]
+            [tempfile.core :refer [tempfile with-tempfile]]))
 
 (defqueries "sql/queries.sql" {:connection (env :db-conn)})
 
-(defn to-html [results]
+(defn map-to-html [data]
   (html [:table
-         [:tr (for [[k v] (first results)]
+         [:tr (for [[k v] (first data)]
                 [:th k])]
-         (for [row results]
+         (for [row data]
            [:tr
             (for [[k v] row]
               [:td v])])]))
 
-(defn mail-html-report [from to subject report spec]
-  (send-message spec {:from from
-                      :to to
-                      :subject subject
-                      :body [{:type "text/html" :content (to-html (report))}]}))
+(defn vec-to-html [data]
+  (html [:table
+         [:tr
+          (for [h (first data)]
+            [:th h])]
+         (for [row (rest data)]
+           [:tr
+            (for [cell row]
+              [:td cell])])]))
+
+(defn stringify-ids [v]
+  (vec (cons (mapv name (first v)) (rest v))))
+
+(defn mail-html-report [account from to subject report]
+  (send-message account {:from from
+                         :to to
+                         :subject subject
+                         :body [{:type "text/html"
+                                 :content (vec-to-html
+                                           (report {} {:as-arrays? true}))}]}))
+
+(defn mail-csv-report [account from to subject report]
+  (with-tempfile [tf
+                  (tempfile
+                   (with-out-str
+                     (write-csv *out* (stringify-ids (report {} {:as-arrays? true})))))]
+    (send-message account {:from from
+                           :to to
+                           :subject subject
+                           :body [{:type :attachment
+                                   :content tf
+                                   :file-name "report.csv"}]})))
 
 (defn -main
   "main"
